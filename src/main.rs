70908b0b77509565179ce127eea3108541069501
윾승도 <66948129+tmdeh@users.noreply.github.com>
2023-05-20 17:31:32 +0900

    입력 스레드 추가

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::thread;
+use std::time::Duration;
+use crossterm::event::{poll, read, Event};
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    // 입력 스레드
+    thread::spawn(||{
+        loop {
+            if poll(Duration::from_millis(500)).unwrap() {
+                match read().unwrap() {
+                    Event::Key(event) => println!("{:?}", event),
+                    _ => println!("Nope"),
+                }
+            }
+        }
+    });
 }
