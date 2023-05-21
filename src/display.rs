c40d8e4b431d4fd6143ec2cbbf52a776d0d67f2a
윾승도 <66948129+tmdeh@users.noreply.github.com>
2023-05-21 13:06:13 +0900

    벽 출력

--- src/display.rs
+++ src/display.rs
@@ -1,20 +1,20 @@
-use std::io::{stdout};
-use crossterm::{execute, terminal, style::{self, Stylize}, QueueableCommand, cursor};
+use std::io::{stdout, Stdout};
+use crossterm::{execute, terminal, style::{self, Stylize}, QueueableCommand, cursor, queue};
 
 
-const X_MAX: i32 = 10;
-const Y_MAX: i32 = 20;
+const X_MAX: i32 = 100;
+const Y_MAX: i32 = 200;
 
 pub struct Window {
-    map: Vec<Vec<i32>>
+    map: Vec<Vec<u16>>
 }
 
 impl Window{
     pub fn new() -> Self {
-        let mut yv: Vec<Vec<i32>> = Vec::new();
+        let mut yv: Vec<Vec<u16>> = Vec::new();
         
         for y in 0..=Y_MAX + 1 {
-            let mut xv: Vec<i32> = Vec::new();
+            let mut xv: Vec<u16> = Vec::new();
             for x in 0..=X_MAX + 2 {
                 if x == 0 || x == X_MAX + 2 || y == Y_MAX + 1 {
                     xv.push(1);
@@ -31,18 +31,16 @@ impl Window{
         }
     }
 
-    pub fn draw(&self) {
-        execute!(stdout(), terminal::Clear(terminal::ClearType::All));
-        
-        for y in self.map.iter() {
-            for x in y.iter() {
-                // TODO 2차원 백터 출력
-                
-                // if y == 1 {
-                //     stdout().queue(style::PrintStyledContent("█".magenta()));
-                // }
+    pub fn draw(&self, stdout: &mut Stdout) {
+        execute!(stdout, terminal::Clear(terminal::ClearType::All));
+
+        for y in 0..self.map.len() {
+            for x in 0 ..self.map[y].len() {
+                if self.map[y][x] == 1 {
+                    queue!(stdout, cursor::MoveTo(x as u16,y as u16), style::PrintStyledContent( "█".magenta()));
+                }
             }
         }
+
     }
 }
-
