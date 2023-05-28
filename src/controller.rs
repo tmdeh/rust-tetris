7afb16cd6bfb8e19ac233de7cbcb3148baec4589
윾승도 <66948129+tmdeh@users.noreply.github.com>
2023-05-28 21:29:51 +0900

    fmt

--- src/controller.rs
+++ src/controller.rs
@@ -1,24 +1,34 @@
-use std::{io::{Stdout, stdout}, time::Duration, process::exit, thread};
+use std::{
+    io::{stdout, Stdout},
+    process::exit,
+    thread,
+    time::Duration,
+};
 
-use crossterm::{event::{poll, read,Event, KeyCode, KeyModifiers}, terminal::enable_raw_mode};
+use crossterm::{
+    event::{poll, read, Event, KeyCode, KeyModifiers},
+    terminal::enable_raw_mode,
+};
 
 pub struct Display {
     stdout: Stdout,
     width: i32,
-    height: i32
+    height: i32,
 }
 
 impl Display {
     pub fn new(w: i32, h: i32) -> Self {
-
         enable_raw_mode().unwrap();
         thread::spawn(Display::key_event);
 
-
-        Display { stdout: stdout(), width: w, height: h }
+        Display {
+            stdout: stdout(),
+            width: w,
+            height: h,
+        }
     }
 
-    pub fn key_event() -> crossterm::Result<()>  {
+    pub fn key_event() -> crossterm::Result<()> {
         loop {
             if poll(Duration::from_millis(500))? {
                 match read()? {
