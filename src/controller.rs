7f6fbb9a3d6b3044ffbed8d68447d92356d14343
윾승도 <66948129+tmdeh@users.noreply.github.com>
2023-05-28 21:24:17 +0900

    key event 분리

---
+++ src/controller.rs
@@ -0,0 +1,52 @@
+use std::{io::{Stdout, stdout}, time::Duration, process::exit, thread};
+
+use crossterm::{event::{poll, read,Event, KeyCode, KeyModifiers}, terminal::enable_raw_mode};
+
+pub struct Display {
+    stdout: Stdout,
+    width: i32,
+    height: i32
+}
+
+impl Display {
+    pub fn new(w: i32, h: i32) -> Self {
+
+        enable_raw_mode().unwrap();
+        thread::spawn(Display::key_event);
+
+
+        Display { stdout: stdout(), width: w, height: h }
+    }
+
+    pub fn key_event() -> crossterm::Result<()>  {
+        loop {
+            if poll(Duration::from_millis(500))? {
+                match read()? {
+                    Event::Key(event) => {
+                        match event.code {
+                            KeyCode::Up => {
+                                println!("Up")
+                            }
+                            KeyCode::Down => {
+                                println!("Down")
+                            }
+                            KeyCode::Left => {
+                                println!("Left")
+                            }
+                            KeyCode::Right => {
+                                println!("Right")
+                            }
+                            KeyCode::Char('c') => {
+                                if event.modifiers == KeyModifiers::CONTROL {
+                                    exit(0x0100);
+                                }
+                            }
+                            _ => (),
+                        };
+                    }
+                    _ => (),
+                };
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,8 @@
-use std::{time::Duration, thread, process::exit};
+mod controller;
 
-use crossterm::{event::{read, Event, poll, KeyCode, ModifierKeyCode, KeyModifiers}, terminal::enable_raw_mode};
+use controller::Display;
 
-fn main() { 
-
-    enable_raw_mode();
-
-    thread::spawn(key_event);
-
-    loop {   
-    }
-}
-
-
-
-fn key_event() -> crossterm::Result<()> {
-    loop {
-        if poll(Duration::from_millis(500))? {
-            match read()? {
-                Event::Key(event) => {
-                    match event.code {
-                        KeyCode::Up => { println!("Up") },
-                        KeyCode::Down => {println!("Down")},
-                        KeyCode::Left => {println!("Left")},
-                        KeyCode::Right => {println!("Right")}, 
-                        KeyCode::Char('c') => {
-                            if event.modifiers == KeyModifiers::CONTROL {
-                                exit(0x0100);
-                            }
-                        },
-                        _ => ()
-                    };
-                },
-                _ => ()
-            };
-        }
-    }
+fn main() {
+    Display::new(100, 300);
+    loop {}
 }
