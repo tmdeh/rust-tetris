ca3927cb44cd6b59743839eb89ed552e14d3348a
윾승도 <66948129+tmdeh@users.noreply.github.com>
2023-07-05 21:49:10 +0900

    출력 변경

--- src/controller/display.rs
+++ src/controller/display.rs
@@ -1,6 +1,6 @@
 use std::{io::{stdout, Write, Stdout}};
 
-use crossterm::{QueueableCommand, cursor, style::{ self }, ExecutableCommand, terminal};
+use crossterm::{QueueableCommand, cursor, ExecutableCommand, terminal, style::{ self } };
 
 pub struct Display {
     map: Vec<Vec<u8>>,
@@ -35,19 +35,35 @@ impl Display {
 
 
     pub fn draw(& mut self) -> std::io::Result<()> {
+        
+        let mut x_delta: u16 = 0;
+
         for (y, line) in self.map.iter().enumerate() {
           for (x, v) in line.iter().enumerate() {
-            self.stdout.queue(cursor::MoveTo(x as u16,y as u16))?;
             if *v == 9 {
-              self.stdout
-                    .queue(style::Print( "\x1b[42m  \x1b[0m"))?;
-            } else {
-                self.stdout
-                    .queue(style::Print( "\x1b[40m  \x1b[0m"))?;
+                print_block(&mut self.stdout, x as u16, y as u16,"\x1b[42m", &mut x_delta).unwrap();
+            }
+            else {
+                print_block(&mut self.stdout, x as u16, y as u16,"\x1b[40m", &mut x_delta).unwrap();
             }
           }
+          x_delta = 0;
         }
         self.stdout.flush()?;
         Ok(())
     }
+}
+
+fn print_block(stdout: &mut Stdout, x: u16, y: u16, s :&str, x_delta: &mut u16) -> std::io::Result<()> {
+    stdout
+        .queue(style::Print(s))?
+        .queue(cursor::MoveTo(x + *x_delta, y))?;
+    *x_delta += 1;
+    stdout
+        .queue(style::Print("  "))?
+        .queue(cursor::MoveTo(x + *x_delta, y))?
+        .queue(style::Print("\x1b[0m"))?
+        .queue(cursor::MoveTo(x + *x_delta, y))?;
+
+    Ok(())
 }
